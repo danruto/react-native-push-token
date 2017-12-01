3ad9910216531b6ac54551616a9cfee7ebaa34f3
Danny Sok <danny@sok.id.au>
2017-12-01 15:42:57 +1100

    Android fixes

--- android/src/main/java/sh/pixelbru/RNPushTokenModule.java
+++ android/src/main/java/sh/pixelbru/RNPushTokenModule.java
@@ -1,6 +1,11 @@
 
 package sh.pixelbru;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -9,35 +14,38 @@ import com.facebook.react.bridge.Promise;
 
 public class RNPushTokenModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
-  private String mDeviceToken = "";
+    private final ReactApplicationContext reactContext;
 
-  public RNPushTokenModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
+    public RNPushTokenModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
 
-    registerNotificationsRegistration();
-  }
+        registerNotificationsRegistration();
+    }
 
-  @Override
-  public String getName() {
-    return "RNPushTokenModule";
-  }
+    @Override
+    public String getName() {
+        return "RNPushTokenModule";
+    }
 
-  @ReactMethod
-  public String getToken(Promise promise) {
-    if (mDeviceToken.isEmpty()) promise.reject();
-    else promise.resolve(mDeviceToken);
-  }
+    @ReactMethod
+    public void getToken(Promise promise) {
+        String token = RNPushTokenSingleton.getInstance().getDeviceToken();
+        if (token.isEmpty())
+            promise.reject("Error", "Error");
+        else
+            promise.resolve(token);
+    }
 
-  private void registerNotificationsRegistration() {
-        IntentFilter intentFilter = new IntentFilter(getReactApplicationContext().getPackageName() + ".RNPushTokenRegistered");
+    private void registerNotificationsRegistration() {
+        IntentFilter intentFilter = new IntentFilter(
+                getReactApplicationContext().getPackageName() + ".RNPushTokenRegistered");
 
         getReactApplicationContext().registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 String token = intent.getStringExtra("token");
-                this.mDeviceToken = token;
+                RNPushTokenSingleton.getInstance().setDeviceToken(token);
             }
         }, intentFilter);
     }
---
+++ android/src/main/java/sh/pixelbru/RNPushTokenSingleton.java
@@ -0,0 +1,29 @@
+package sh.pixelbru;
+
+/**
+ * Created by danny on 1/12/17.
+ */
+
+public class RNPushTokenSingleton {
+    private static RNPushTokenSingleton instance;
+    private String deviceToken;
+
+    private RNPushTokenSingleton() {
+    }
+
+    public static synchronized RNPushTokenSingleton getInstance() {
+        if (instance == null) {
+            instance = new RNPushTokenSingleton();
+        }
+
+        return instance;
+    }
+
+    public String getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(String value) {
+        this.deviceToken = value;
+    }
+}
